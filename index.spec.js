62e581fadd3f43a2c47b7fddecec1a53d56b5ced
Simone Lusenti <simone@plasticpanda.com>
2016-12-04 18:11:37 +0100

    working tests

--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,23 @@
 
-import test from 'tape';
+import tap from 'tap';
 
-import { PATHNAME_RE, replaceOne, replaceAll, compare } from './pathmatch';
+import { PATHNAME_RE, replaceOne, replaceAll, compare } from './index';
 
-test('replaceOne should replace one {} group with a regexp', t => {
+tap.test('replaceOne should replace one {} group with a regexp', t => {
   t.deepEqual(replaceOne('/foo/{bar}/baz'), [9, 'bar', `/foo/(${PATHNAME_RE})/baz`]);
   t.deepEqual(replaceOne('/foo/{bar}/{baz}'), [9, 'bar', `/foo/(${PATHNAME_RE})/{baz}`]);
   t.deepEqual(replaceOne('/foo/{bar}/{baz+}'), [9, 'bar', `/foo/(${PATHNAME_RE})/{baz+}`]);
   t.end();
 });
 
-test('replaceAll should replace all {} groups with regexps', t => {
+tap.test('replaceAll should replace all {} groups with regexps', t => {
   t.deepEqual(replaceAll('/foo/{bar}/baz'), [`/foo/(${PATHNAME_RE})/baz`, ['bar']]);
   t.deepEqual(replaceAll('/foo/{bar}/{baz}'), [`/foo/(${PATHNAME_RE})/(${PATHNAME_RE})`, ['bar', 'baz']]);
   // t.deepEqual(replaceAll('/foo/{bar}/{baz+}'), [`/foo/(${PATHNAME_RE})/(${PATHNAME_RE})`, ['bar', 'baz+']]);
   t.end();
 });
 
-test('compare', t => {
+tap.test('compare', t => {
   t.deepEqual(compare('/foo/bar', '/foo/bar'), [[], []]);
   t.deepEqual(compare('/foo/bar', '/foo'), false);
   t.deepEqual(compare('/foo/bar', '/foo/bar/baz'), false);
